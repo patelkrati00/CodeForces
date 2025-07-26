56cbf17f47546c5bcb9faab6ff385e3da05f1e04
patelkrati00 <patelkrati00@gmail.com>
2025-07-26 15:48:58 +0530

    In the search of easy problem

---
+++ SearchOfEasyProblem.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SearchOfEasyProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+           if(arr[i]==1){
+            System.out.println("HARD");
+            return;
+           }
+        }
+        
+            System.out.println("EASY");
+
+    }
+}
